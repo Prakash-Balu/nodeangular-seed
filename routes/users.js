841c3fe077b0118dda8b0ba8325e40d356c2d2ec
Prakash-Balu <prakash.engg89@gmail.com>
2017-06-23 15:07:01 +0530

    updated

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'test'
-});
-
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-var username= req.body.username;
-  var password = req.body.password;
-  connection.query('SELECT * FROM user WHERE name = ?',[username], function (error, results, fields) {
-	  console.log(results.length);
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    // console.log('The solution is: ', results);
-    if(results.length >0){
-      if(results[0].password == password){
-        res.send({
-          "code":200,
-          "message":"login sucessfull"
-            });
-      }
-      else{
-        res.send({
-          "code":204,
-          "message":"Username and password does not match"
-            });
-      }
-    }
-    else{
-      res.send({
-        "code":204,
-        "message":"Username does not exists"
-          });
-    }
-  }
-  });
+	var username= req.body.username;
+	var password = req.body.password;
+	req.getConnection(function(err,connection){
+		connection.query('SELECT * FROM user WHERE name = ?',[username], function (error, results, fields) {
+			console.log(results.length);
+			if (error) {
+				// console.log("error ocurred",error);
+				res.send({
+					"code":400,
+					"failed":"error ocurred"
+				})
+			} else {
+				// console.log('The solution is: ', results);
+				if(results.length >0){
+					if(results[0].password == password){
+						res.send({
+							"code":200,
+							"message":"login sucessfull"
+						});
+					} else {
+						res.send({
+							"code":204,
+							"message":"Username and password does not match"
+						});
+					}
+				} else {
+					res.send({
+						"code":204,
+						"message":"Username does not exists"
+					});
+				}
+			}
+		});
+	});
 });
 
 //module.exports = router;
