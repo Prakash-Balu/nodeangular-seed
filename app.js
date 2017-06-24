02b70867cb13270bb40a3db959f75940bda627c1
prakash <prakash.engg89@gmail.com>
2017-06-24 16:13:32 +0530

    changes committed

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(
     connection(mysql,{
         host: 'localhost',
         user: 'root',
-        password : 'root',
+        password : '',
         port : 3306, //port mysql
         database:'test'
     })
